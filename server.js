be1571854ccae37adb975beb9f989afb62e5aacf
ali mert inal <mert.inal@gmail.com>
2024-03-23 21:42:55 +0100

    feat: new ToDo

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 
 let app = express();
 
 app.use(express.static("public"));
+app.use(urlencoded({extended: false}));
 
 app.get("/todos",async (req,res)=>{
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await response.json();
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
+    let data = await response.json();
+    data = [...customTodo,...data]
     return res.send(
         `
         <ul>
@@ -14,6 +16,13 @@ app.get("/todos",async (req,res)=>{
         </ul>
         `
     )
+})
+
+let customTodo = []
+
+app.post("/todos",async (req,res)=>{
+    customTodo  = [ {title: req.body.title, completed: false},...customTodo];    
+    return res.status(204).send("post request")
 })
 
 app.listen(3000, ()=>{
